e03ba11c84745389ec91c90ff27482f8aa0e4d91
Aaron McCurry <a.mccurry1@andersonuniversity.edu>
2026-03-04 10:02:23 -0500

    Created db setup, Track model, and seeded db

---
+++ database/setup.js
@@ -0,0 +1,59 @@
+const { Sequelize, DataTypes } = require("sequelize");
+require('dotenv').config();
+
+const db = new Sequelize({ 
+    dialect: 'sqlite', 
+    storage: `database/${process.env.DB_NAME}` || 'database/music_library.db', 
+    logging: console.log
+})
+
+async function setupDatabase() { 
+    try { 
+        await db.authenticate(); 
+        console.log('Connection to database established successfully.'); 
+
+        await db.sync({ force: true })
+        console.log('Database file created at:',
+     `database/${process.env.DB_NAME}`); 
+
+        await db.close(); 
+    } catch (error) { 
+         console.error('Unable to connect to the database:', error); 
+    } 
+}
+
+const Track = db.define('Track', {
+    trackId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    songTitle: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    artistName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    albumName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    duration: {
+        type: DataTypes.INTEGER
+    },
+    releaseYear: {
+        type: DataTypes.INTEGER,
+    }
+});
+
+module.exports = { db, Track };
+
+if (require.main === module) {
+    setupDatabase();
+}
